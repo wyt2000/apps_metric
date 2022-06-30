8d4f7af91fd44de20db8e716cf2c815fbdc7ed65
loubna ben allal <loubna@huggingface.co>
2022-06-30 23:47:41 +0000

    Update apps_metric.py

--- apps_metric.py
+++ apps_metric.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TODO: Add a description here."""
+"""APPS metric for the evaluation of code generation"""
 
 import evaluate
 import datasets
