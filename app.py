7106653a6a9740105d4148e58ac517a71f1acf91
loubnabnl <loubna@huggingface.co>
2022-07-04 12:28:59 +0000

    update metric name

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import evaluate
 from evaluate.utils import launch_gradio_widget
 
 
-module = evaluate.load("loubnabnl/apps-metric")
+module = evaluate.load("loubnabnl/apps_metric")
 launch_gradio_widget(module)
