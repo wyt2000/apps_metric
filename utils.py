4b9354a49607a76a9303e9d07b0e37911667d8da
loubna ben allal <loubna@huggingface.co>
2022-07-15 15:38:15 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import itertools
 import numpy as np
 from typing import Dict
 from datasets import load_dataset
-import .testing_util as run_test
+from .testing_util import run_test
 
 
 DATASET = "codeparrot/apps"
